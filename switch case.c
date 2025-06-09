f31d462013f8d5dfe460f69c48101cab77001b1a
Wasu <64682548+Officerwasu@users.noreply.github.com>
2025-06-09 15:39:17 +0530

    Create switch case.c

---
+++ switch case.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+int vowels;
+printf("Enter Vowel number(1 to 5): ");
+scanf("%d",&vovels);
+switch(vowels)
+{
+case 1:
+printf("A");
+break;
+
+case 2:
+printf("E");
+break;
+
+case 3:
+printf("I");
+break;
+
+case 4:
+printf("O");
+break;
+
+case 5:
+printf("U");
+break;
+
+default:
+printf("There are only 5 Vowel");
+}
+return 0;
+}
